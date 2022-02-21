d476d16a149cf53e9f31fcd38643499452f4c7b7
David-VR0 <david.97.vargas@outlook.com>
2022-02-20 20:28:45 -0600

    Double, letras

--- interfaz.py
+++ interfaz.py
@@ -2,19 +2,28 @@ import tkinter as tk
 from tkinter import *
 import os, sys
 import montante
+
+def borrar():
+    etiquetar1 = tk.Label(text="                            ", font=("Arial", 14), fg='#FF0000' )
+    etiquetar1.place(x=80, y=210)
+    etiquetar2 = tk.Label(text="                            ", font=("Arial", 14), fg='#FF0000')
+    etiquetar2.place(x=80, y=240)
+    etiquetar3 = tk.Label(text="                            ", font=("Arial", 14), fg='#FF0000')
+    etiquetar3.place(x=80, y=270)
 def antr():
+    borrar()
     matriz=[]
     matriz=[[ax.get(),ay.get(),az.get(),1,0,0],[bx.get(), by.get(), bz.get(),0,1,0],[cx.get(), cy.get(), cz.get(),0,0,1]]
     a = ag.get()
     b = bg.get()
     c = cg.get()
     x1,x2,x3 = montante.resolver(matriz,a,b,c)
-    etiquetar1 = tk.Label(text="{0:.6f}".format(x1), font=("Arial", 10), fg='#FF0000' )
-    etiquetar1.place(x=60, y=210)
-    etiquetar2 = tk.Label(text="{0:.6f}".format(x2), font=("Arial", 10), fg='#FF0000')
-    etiquetar2.place(x=60, y=240)
-    etiquetar3 = tk.Label(text="{0:.6f}".format(x3), font=("Arial", 10), fg='#FF0000')
-    etiquetar3.place(x=60, y=270)
+    etiquetar1 = tk.Label(text="{0:.6f}".format(x1), font=("Arial", 14), fg='#FF0000' )
+    etiquetar1.place(x=80, y=210)
+    etiquetar2 = tk.Label(text="{0:.6f}".format(x2), font=("Arial", 14), fg='#FF0000')
+    etiquetar2.place(x=80, y=240)
+    etiquetar3 = tk.Label(text="{0:.6f}".format(x3), font=("Arial", 14), fg='#FF0000')
+    etiquetar3.place(x=80, y=270)
 
 
 
@@ -26,18 +35,18 @@ root.resizable(0,0)
 root.geometry("400x300+500+100")
 
 #variable de entrada
-ax = tk.IntVar()
-ay = tk.IntVar()
-az = tk.IntVar()
-ag = tk.IntVar()
-bx = tk.IntVar()
-by = tk.IntVar()
-bz = tk.IntVar()
-bg = tk.IntVar()
-cx = tk.IntVar()
-cy = tk.IntVar()
-cz = tk.IntVar()
-cg = tk.IntVar()
+ax = tk.DoubleVar()
+ay = tk.DoubleVar()
+az = tk.DoubleVar()
+ag = tk.DoubleVar()
+bx = tk.DoubleVar()
+by = tk.DoubleVar()
+bz = tk.DoubleVar()
+bg = tk.DoubleVar()
+cx = tk.DoubleVar()
+cy = tk.DoubleVar()
+cz = tk.DoubleVar()
+cg = tk.DoubleVar()
 
 m = Canvas(root, width=200, height=300)
 m.pack(fill="x")
@@ -115,10 +124,10 @@ entradai2.place(x=215,y=160)
 boton1 = tk.Button(text="Iniciar", command=antr)
 boton1.place(x=280,y=130)
 
-etiqueta11 = tk.Label(text="x1 =", font=("Arial", 10))
+etiqueta11 = tk.Label(text="x1 =", font=("Arial", 14))
 etiqueta11.place(x=30,y=210)
-etiqueta12 = tk.Label(text="x2 =", font=("Arial", 10))
+etiqueta12 = tk.Label(text="x2 =", font=("Arial", 14))
 etiqueta12.place(x=30,y=240)
-etiqueta13 = tk.Label(text="x3 =", font=("Arial", 10))
+etiqueta13 = tk.Label(text="x3 =", font=("Arial", 14))
 etiqueta13.place(x=30,y=270)
 root.mainloop()
--- montante.py
+++ montante.py
@@ -7,33 +7,34 @@ def resolver(matriz,a1,a2,a3):
     pivAnt = 1
     print("\n")
     print("Proceso")
-    for pivN in range(0, 3):
+    for pivAct in range(0, 3):
         print("\n")
-        for r in range(0, 3):
+        for f in range(0, 3):
             
             for c in range(0, 6):
-                if c != pivN and r != pivN:
-                    matriz[r][c] = int(round((matriz[pivN][pivN] * matriz[r][c] - matriz[r][pivN] * matriz[pivN][c]) / pivAnt))
-            if r != pivN:
-                matriz[r][pivN] = 0
+                if c != pivAct and f != pivAct:
+                    matriz[f][c] = float(round((matriz[pivAct][pivAct] * matriz[f][c] - matriz[f][pivAct] * matriz[pivAct][c]) / pivAnt))
+            if f != pivAct:
+                matriz[f][pivAct] = 0
             
-        print("\n".join([''.join(['{:4}'.format(item) for item in row]) for row in matriz]))
+        print("\n".join([''.join(['{:6}'.format(item) for item in row]) for row in matriz]))
 
         '''for mat in matriz:'''
         ''' print(matriz)'''
         '''print(str(matriz))'''
-        pivAnt = matriz[pivN][pivN]
+        pivAnt = matriz[pivAct][pivAct]
             
     print()
     print("Matriz final\n")
-    print("\n".join([''.join(['{:4}'.format(item) for item in row]) for row in matriz]))
+    print("\n".join([''.join(['{:6}'.format(item) for item in row]) for row in matriz]))
     det = matriz[0][0]
+    print("\n Determinante:",det)
     x = a1 * matriz[0][3] / det + a2 * matriz[0][4] / det + a3 * matriz[0][5] / det
     y = a1 * matriz[1][3] / det + a2 * matriz[1][4] / det + a3 * matriz[1][5] / det
     z = a1 * matriz[2][3] / det + a2 * matriz[2][4] / det + a3 * matriz[2][5] / det
     
-    print("\nValores de x,y,z:")
-    print("X = " + str(x))
-    print("Y = " + str(y))
-    print("Z = " + str(z))
+    print("\nValores de x1,x2,x3:")
+    print("X1 = " + str(x))
+    print("X2 = " + str(y))
+    print("X3 = " + str(z))
     return x,y,z
